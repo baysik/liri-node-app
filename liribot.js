f23bcc13bb7f5aa413bfb7f126968f707c176aaa
baysik <iambaysik@gmail.com>
2019-07-12 17:06:17 -0600

    added packages

---
+++ liribot.js
@@ -0,0 +1,8 @@
+// import axios
+let axios = require("axios");
+// import spotify
+let Spotify = require("node-spotify-api");
+// import moment
+let moment = require("moment");
+// import dotenv;
+let dotEnv = require("dotenv").config();
