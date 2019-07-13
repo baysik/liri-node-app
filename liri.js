2e7925f7b5fbf7d5d011cf2c2d45d173461169e7
baysik <iambaysik@gmail.com>
2019-07-12 21:11:05 -0600

    fixed minor formatting error

--- liri.js
+++ liri.js
@@ -11,19 +11,29 @@ let moment = require("moment");
 const spotify = new Spotify(keys.spotify);
 
 // console.log(spotify);
-
-let userInput = process.argv[3]
+// var searchParameter = process.argv[2]
+var userInput = ""
+var concertArtist = ""
 
 if (process.argv[2] === "concert-this"){
+    for (var i=3; i < process.argv.length; i++){
+        console.log(process.argv[i])
+        userInput = userInput + process.argv[i];
+        concertArtist = concertArtist + " " + process.argv[i]
+    }
     var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
     axios.get(queryURL)
     .then(function(response){
+        console.log(response)
         let venueName = response.data[0].venue.name
         let venueCity = response.data[0].venue.city
         let venueRegion = response.data[0].venue.region
-        console.log(userInput + " will be playing at " + "'" + venueName + "'" + " in " + venueCity + ", " + venueRegion)
+        console.log(concertArtist + " will be playing at " + "'" + venueName + "'" + " in " + venueCity + ", " + venueRegion)
     })
 }else if(process.argv[2] === "spotify-this-song"){
+    for (var i=3; i < process.argv.length; i++){
+        userInput = userInput + " " + process.argv[i];
+    }
     spotify.search({
         type: "track",
         query: userInput,
